1fd549a37ae8c24dfb2e8860d99dc8f6404e5c7b
Colin McFadden <cmcfadden@gmail.com>
2017-08-24 15:43:47 -0500

    remove ansible checkin

--- ansible/roles/apache/test/integration/enable-mod-ssl/default_spec.rb
+++
@@ -1,9 +0,0 @@
-describe service('httpd') do
-  it { should be_enabled }
-  it { should be_installed }
-  it { should be_running }
-end
-
-describe command('httpd -t -D DUMP_MODULES') do
-  its('stdout') { should match /\sssl_module\s/ }
-end
